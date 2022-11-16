ca156abe563c5c0133652253a810d80a24688a6b
marc aschwanden <marc.aschwanden88@gmail.com>
2022-11-16 21:56:54 +0100

    small fixes offers and file-naming

--- landing.js
+++
@@ -1,6 +0,0 @@
-const logo = document.querySelector('.logo');
-
-logo.addEventListener('click', () => {
-    window.open('index.html', '_self');
-    console.log('hi')
-})
---
+++ script.js
@@ -0,0 +1,6 @@
+const logo = document.querySelector('.logo');
+
+logo.addEventListener('click', () => {
+    window.open('landingpage.html', '_self');
+    console.log('hi')
+})
